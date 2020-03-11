78a44bc0f19682c5d853f686f0cdf29331f6263d
DanteK15 <44585739+DanteK15@users.noreply.github.com>
2020-03-10 23:42:15 -0700

    finished fuzzy cmeans

--- programming3-fuzzycmeans.py
+++ programming3-fuzzycmeans.py
@@ -11,8 +11,8 @@ def l2NormNoSqrt(a, b):
     return ((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2)
 
 
-k = 3
-r = 1
+k = 6
+r = 10
 m = 2
 data = np.genfromtxt("cluster_dataset.txt", delimiter="  ")
 rowCount = data.shape[0]
@@ -20,6 +20,8 @@ columnCount = data.shape[1]
 maxRand = np.amax(data)
 minRand = np.amin(data)
 
+finalSSE = float("inf")
+finalIterations = []
 for i in range(r):
     sse = float("inf")
     # generate random intial membership grades
@@ -30,6 +32,7 @@ for i in range(r):
         rowSum = sum(memGrades[i])
         memGrades[i] = memGrades[i] / rowSum
 
+    iterations = []
     improving = True
     while improving:
         # example he gave
@@ -40,7 +43,6 @@ for i in range(r):
         k = 2
         m = 2
         """
-
         # calculate new centroids
         centroids = np.sum(data * np.transpose((memGrades ** m))[:, :, None], axis=1) / np.sum(np.transpose(memGrades) ** m, axis=1)[:, np.newaxis]
 
@@ -63,4 +65,29 @@ for i in range(r):
         if sse - sseTotal < 0.1 and not(sse == float("inf")):
             improving = False
         sse = sseTotal
-        print(sseTotal)
+
+        assignedCluster = np.argmax(memGrades, axis=1)
+        # split up data based on cluster
+        dataGroups = []
+        for i in range(k):
+            dataGroups.append(data[assignedCluster == i])
+        dataGroups = np.asarray(dataGroups)
+        # save all iterations of the centroids moving
+        iterations.append((centroids, dataGroups))
+        # print(sseTotal)
+
+    if finalSSE > sse:
+        print("yay changed")
+        finalSSE = sse
+        finalIterations = copy.deepcopy(iterations)
+
+# print best iteration group
+for i in range(len(finalIterations)):
+    for j in range(k):
+        color = [[(j % k) / k, (j * 2 % k) / k, (j * 3 % k) / k]]
+        plt.scatter(finalIterations[i][1][j][:, 0], finalIterations[i][1][j][:, 1], c=color)
+
+    crosses = plt.scatter(finalIterations[i][0][:, 0], finalIterations[i][0][:, 1], c="red", marker="X")
+    plt.legend([crosses], ["centroids"], scatterpoints=1, loc='lower right')
+    plt.savefig("outputs/output" + str(i))
+    plt.clf()
